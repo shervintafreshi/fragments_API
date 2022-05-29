f49f069e2ee33cfca1ccf676a06d2fb39334de83
shervintafreshi <shervintafreshipour@gmail.com>
2022-05-29 13:57:03 -0400

    Modified CI yaml file

---
+++ tests/unit/app.test.js
@@ -0,0 +1,10 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+// If the request targets a non-existent route, a 404 status code should be returned
+describe('GET /*', () => {
+  test('Route does not exist', () => request(app).get('/*').expect(404));
+});
