9ea1c1b62758c72247f7b90b1f98cb906a3e4d35
shervintafreshi <shervintafreshipour@gmail.com>
2022-06-26 17:31:50 -0400

    added additional test for fragment metadata format

--- tests/unit/get.id.info.test.js
+++ tests/unit/get.id.info.test.js
@@ -13,7 +13,7 @@ describe('GET /v1/fragments/:id/info', () => {
       .auth('invalid@email.com', 'incorrect_password')
       .expect(401));
 
-  // Using a valid username/password pair should give return HTTP 200 response
+  // Using a valid username/password pair and fragment id should return HTTP 200 response
   test('authenticated requests return a HTTP 200 Response', async () => {
     const res1 = await request(app)
       .post('/v1/fragments')
@@ -26,5 +26,27 @@ describe('GET /v1/fragments/:id/info', () => {
       .auth('user1@email.com', 'password1');
     expect(res2.statusCode).toBe(200);
     expect(res2.body.status).toBe('ok');
+  });
+
+  // Using a valid username/password pair and fragment id should return fragment metadata
+  test('valid authenticated requests returns properly formatted fragment metadata', async () => {
+    const res1 = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is a text fragment');
+
+    const res2 = await request(app)
+      .get(`/v1/fragments/${res1.body.fragment.id}/info`)
+      .auth('user1@email.com', 'password1');
+    expect(res2.statusCode).toBe(200);
+    expect(res2.body.status).toBe('ok');
+    expect(res2.body).toHaveProperty('fragment');
+    expect(res2.body.fragment).toHaveProperty('id');
+    expect(res2.body.fragment).toHaveProperty('created');
+    expect(res2.body.fragment).toHaveProperty('updated');
+    expect(res2.body.fragment).toHaveProperty('ownerId');
+    expect(res2.body.fragment).toHaveProperty('type');
+    expect(res2.body.fragment).toHaveProperty('size');
   });
 });
