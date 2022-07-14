4b59fa53ee74d52899e19b937b8f64f68093c546
shervintafreshi <shervintafreshipour@gmail.com>
2022-07-14 17:41:47 -0400

    Updated the 'GET /fragments' route to accept query parameter

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,15 +1,30 @@
 // src/routes/api/get.js
 const { createSuccessResponse } = require('../../../src/response');
+const { createErrorResponse } = require('../../../src/response');
+
+const Fragment = require('../../model/fragment');
+const crypto = require('crypto');
 
 /**
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
-  // Generate a success response
-  const responseData = createSuccessResponse({
-    fragments: [],
-  });
+  // Verify if user requested full metadata
+  const expand = req.query.expand == 1;
+  const ownerId = crypto.createHash('sha256').update(req.user).digest('base64');
 
-  // Send a 200 'OK' response
-  res.status(200).json(responseData);
+  // Query the database
+  Fragment.byUser(ownerId, expand)
+    .then((fragments) => {
+      // Send a 200 'OK' response
+      const responseData = createSuccessResponse({
+        fragments,
+      });
+      res.status(200).json(responseData);
+    })
+    .catch(() => {
+      // Send a 404 'error' response
+      const responseData = createErrorResponse(404, 'Resource not found');
+      res.status(404).json(responseData);
+    });
 };
