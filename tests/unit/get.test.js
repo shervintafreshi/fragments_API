5b63ffe0eb0f3a23f61a1f1c2019d1d4cdf8483c
shervintafreshi <shervintafreshipour@gmail.com>
2022-07-20 17:51:23 -0400

    Added additional unit tests for 'GET /v1/fragments' route

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -20,5 +20,36 @@ describe('GET /v1/fragments', () => {
     expect(Array.isArray(res.body.fragments)).toBe(true);
   });
 
-  // TODO: we'll need to add tests to check the contents of the fragments array later
+  // Using a valid username/password pair to retrieve several stored fragments should work
+  test('authenticated requests return a fragments array with the valid elements', async () => {
+    const res1 = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is a text fragment');
+
+    const res2 = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+    expect(res2.statusCode).toBe(200);
+    expect(res2.body.status).toBe('ok');
+    expect(Array.isArray(res2.body.fragments)).toBe(true);
+    expect(res2.body.fragments.length).toBe(1);
+    expect(res2.body.fragments[0]).toBe(res1.body.fragment.id);
+  });
+
+  // Using a valid username/password pair to retrieve several stored fragments with the expand option set should work
+  test('authenticated requests with the expand option set return a fragments array with the valid elements', async () => {
+    const res2 = await request(app)
+      .get('/v1/fragments?expand=1')
+      .auth('user1@email.com', 'password1');
+    expect(res2.statusCode).toBe(200);
+    expect(res2.body.status).toBe('ok');
+    expect(Array.isArray(res2.body.fragments)).toBe(true);
+    expect(res2.body.fragments.length).toBe(1);
+    expect(res2.body.fragments[0]).toHaveProperty('id');
+    expect(res2.body.fragments[0]).toHaveProperty('ownerId');
+    expect(res2.body.fragments[0]).toHaveProperty('created');
+    expect(res2.body.fragments[0]).toHaveProperty('updated');
+    expect(res2.body.fragments[0]).toHaveProperty('type');
+    expect(res2.body.fragments[0]).toHaveProperty('size');
+  });
 });
